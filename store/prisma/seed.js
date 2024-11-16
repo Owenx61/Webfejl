25486e3212dbb90331b34782c0113517ee1d3af6
Gábor Sütő <g.suto79@gmail.com>
2024-11-16 17:35:46 +0100

    Adding products to the database

---
+++ store/prisma/seed.js
@@ -0,0 +1,21 @@
+/*ez tölti fel a databaset data elég h csak a productal legyen egyenlő mert a db attributumai ugyanazok mitn a product objektumé */
+const { PrismaClient } = require('@prisma/client');
+const products = require('./products.json');
+const prisma = new PrismaClient();
+
+async function main() {
+  for (const product of products) {
+    await prisma.product.create({
+      data: product,
+    });
+  }
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
