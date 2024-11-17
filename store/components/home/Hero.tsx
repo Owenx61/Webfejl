62fabb9b379a2a9132ef79f7973355659e021a84
Gábor Sütő <g.suto79@gmail.com>
2024-11-17 17:58:04 +0100

    Finishing the content of the Homepage

--- store/components/home/Hero.tsx
+++ store/components/home/Hero.tsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import HeroCarousel from "./HeroCarousel";
+
 function Hero() {
-  return <div>Hero</div>;
+  return <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+    <div>
+      <h1 className="max--w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
+        We are changing the way people shop
+      </h1>
+      <p className="mt-8 max-w-xl text-lg leading-8 text-muted-foreground">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi consectetur voluptatem beatae. Accusamus accusantium quisquam, quos temporibus rem ipsam vel!
+      </p>
+      <Button asChild size='lg' className="mt-10">
+        <Link href='/products'>Our products</Link>
+      </Button>
+    </div>
+    <HeroCarousel/>
+  </section>;
 }
 
 export default Hero;
